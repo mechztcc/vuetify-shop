1acc0d745ff38aaacb478335c6ada6dcc7472a1e
mechztcc <dojathan@gmail.com>
2023-08-14 10:32:39 -0300

    feat: added FilterByMark

---
+++ src/modules/home/components/FilterByMark.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-checkbox :label="item" v-for="(item, index) of items" :key="index" />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "FilterByMarkComponent",
+  data() {
+    return {
+      items: ["Samsung", "Xiaomi", "Motorola", "Apple", "Nokia"],
+    };
+  },
+};
+</script>
+<style lang=""></style>
--- src/modules/home/components/FiltersContainer.vue
+++ src/modules/home/components/FiltersContainer.vue
@@ -2,19 +2,23 @@
   <v-card class="pa-3">
     <FilterByPriceRange />
     <FiltersByCategory />
+    <FilterByMark />
   </v-card>
 </template>
 <script lang="ts">
 import FilterByPriceRange from "./FilterByPriceRange.vue";
 import FiltersByCategory from "./FiltersByCategory.vue";
+import FilterByMark from "./FilterByMark.vue";
 export default {
   name: "FiltersContainerComponent",
   components: {
     FilterByPriceRange,
     FiltersByCategory,
+    FilterByMark,
   },
   data() {
-    return {};
+    return {
+    };
   },
 };
 </script>
