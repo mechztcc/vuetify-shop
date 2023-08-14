8f9ae399acb79b64c3262508a6ee523be3935464
mechztcc <dojathan@gmail.com>
2023-08-14 10:28:00 -0300

    feat: added FiltersByCategory

---
+++ src/modules/home/components/FiltersByCategory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-combobox
+      chips
+      dense
+      variant="outlined"
+      label="Categoria"
+      :items="items"
+      multiple
+    ></v-combobox>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "FiltersByCategoryComponent",
+  data() {
+    return {
+      items: ["Headphone", "Smartphone", "Mouse"],
+    };
+  },
+};
+</script>
+<style lang="scss"></style>
--- src/modules/home/components/FiltersContainer.vue
+++ src/modules/home/components/FiltersContainer.vue
@@ -1,14 +1,17 @@
 <template>
   <v-card class="pa-3">
     <FilterByPriceRange />
+    <FiltersByCategory />
   </v-card>
 </template>
 <script lang="ts">
 import FilterByPriceRange from "./FilterByPriceRange.vue";
+import FiltersByCategory from "./FiltersByCategory.vue";
 export default {
   name: "FiltersContainerComponent",
   components: {
     FilterByPriceRange,
+    FiltersByCategory,
   },
   data() {
     return {};
