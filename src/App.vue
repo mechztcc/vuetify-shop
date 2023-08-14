fe42ba91cce73f651e5aeba5f287e890263f7a25
mechztcc <dojathan@gmail.com>
2023-08-14 10:49:50 -0300

    feat: created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <NavbarVue />
   <router-view />
 </template>
 
 <script lang="ts" setup>
-  //
+import NavbarVue from "./modules/shared/components/navbar.vue";
+components: {
+  NavbarVue;
+}
 </script>
--- src/modules/shared/components/navbar.vue
+++ src/modules/shared/components/navbar.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1>navbar</h1>
-  </div>
+  <v-card>
+    <div class="d-flex justify-space-between pa-6 bg-primary">
+      Logo
+      <v-icon>mdi-menu</v-icon>
+    </div>
+  </v-card>
 </template>
 <script lang="ts">
 export default {
